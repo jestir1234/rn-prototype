43bec7fc2c7473e8c67afc159088ca1a2aa626c1
Sheraz Ahmad Khilji <sheraz.khilji@rocket-internet.de>
2018-05-04 20:12:16 +0200

    Successfully added and ran detox

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -2,18 +2,20 @@ describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+
+  it('Fields are visible', async () => {
+    await expect(element(by.id('UsernameTestId'))).toBeVisible();
+    await expect(element(by.id('PasswordTestId'))).toBeVisible();
+    await expect(element(by.id('LoginTestId'))).toBeVisible();
   });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+
+  it('Perform invalid Login', async () => {
+    await element(by.id('UsernameTestId')).typeText('username');
+    await element(by.id('PasswordTestId')).typeText('password');
+    await element(by.id('LoginTestId')).tap();
   });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+
+  it('Some Sample test', async () => {
+
   });
 })

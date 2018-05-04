ac3a9673a868f5d97d2427848aad989cae443bdb
Sheraz Ahmad Khilji <sheraz.khilji@rocket-internet.de>
2018-05-04 20:30:45 +0200

    Hid Keyboard when trying to login

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,16 +3,17 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
+
+  it('Perform invalid Login', async () => {
+    await element(by.id('UsernameTestId')).typeText('username');
+    await element(by.id('PasswordTestId')).typeText('password\n');
+    await element(by.id('LoginTestId')).tap();
+
+  });
   it('Fields are visible', async () => {
     await expect(element(by.id('UsernameTestId'))).toBeVisible();
     await expect(element(by.id('PasswordTestId'))).toBeVisible();
     await expect(element(by.id('LoginTestId'))).toBeVisible();
-  });
-
-  it('Perform invalid Login', async () => {
-    await element(by.id('UsernameTestId')).typeText('username');
-    await element(by.id('PasswordTestId')).typeText('password');
-    await element(by.id('LoginTestId')).tap();
   });
 
   it('Some Sample test', async () => {

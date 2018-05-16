7cfeba9e62a467632a63f293b16c5cd1d9ccff36
Sheraz Ahmad Khilji <sheraz.khilji@rocket-internet.de>
2018-05-16 13:09:28 +0200

    Reverted reducers index.js and updated fastlane to remove node_modules directory

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,7 @@ import mealReducer from './meals.js'
 import userReducer from './user.js'
 import deliveryReducer from './delivery.js'
 import { persistReducer } from 'redux-persist'
-import autoMergeLevel1 from 'redux-persist/lib/stateReconciler/autoMergeLevel1'
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
 const ROOT_STORAGE_KEY = 'ROOT_STORAGE_KEY'
@@ -13,14 +12,14 @@ const USER_INFO_STORAGE_KEY = 'USER_INFO_STORAGE_KEY'
 const userPersistConfig = {
   key: USER_INFO_STORAGE_KEY,
   storage,
-  stateReconciler: autoMergeLevel1,
+  stateReconciler: hardSet,
   whitelist: ['userInfo']
 }
 
 const rootPersistConfig = {
   key: ROOT_STORAGE_KEY,
   storage,
-  stateReconciler: autoMergeLevel2,
+  stateReconciler: hardSet,
   whitelist: ['user']
 }
 

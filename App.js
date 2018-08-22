df119fb93a410eccf203ec990b888824757cf7ac
matt.duy.nguyen <jestir1234@gmail.com>
2018-08-22 15:53:20 +0200

    pushing some shit

--- App.js
+++ App.js
@@ -4,28 +4,48 @@ import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = { email: "", password: "" };
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logoStyles}
+            source={{
+              uri: "https://cdn.goreadymade.com/ml/goreadymade-logo.svg"
+            }}
+          />
+        </View>
         <View style={styles.heroContainer}>
           <Image
             style={styles.heroImage}
             source={{
               uri:
-                "https://images.goreadymade.com/f_auto,fl_lossy,q_auto/goreadymade_website/homepage/mobile_hero_top.jpg"
+                "https://images.goreadymade.com/f_auto,fl_lossy,q_auto/goreadymade_website/homepage/banner_3-12-18-bigger.jpg"
             }}
           />
         </View>
-        <View style={styles.inputContainer}>
-          <Text>EMAIL</Text>
-          <TextInput
-            style={styles.inputStyles}
-            onChangeText={text => this.setState({ text })}
-            value={this.state.text}
-          />
+
+        <View style={styles.loginContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputStyles}
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+              placeholder={"EMAIL"}
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputStyles}
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+              placeholder={"PASSWORD"}
+            />
+          </View>
         </View>
       </View>
     );
@@ -35,6 +55,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#fff",
     paddingTop: 30,
     paddingLeft: 10,
@@ -42,30 +66,45 @@ const styles = StyleSheet.create({
   },
   loginContainer: {
     display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     flexDirection: "column",
-    height: 200,
-    width: 300,
-    backgroundColor: "green"
+    width: 200,
+    height: 200
   },
   heroContainer: {
     alignSelf: "stretch",
     width: "100%",
-    height: 56
+    height: "100%",
+    zIndex: -1,
+    position: "absolute"
   },
   heroImage: {
     width: "100%",
-    height: 56
+    height: "100%",
+    zIndex: -1
   },
   inputContainer: {
     display: "flex",
-    flexDirection: "row",
-    alignItems: "center"
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    margin: 10,
+    backgroundColor: "white"
   },
   inputStyles: {
-    width: 200,
+    width: 220,
     height: 30,
     borderColor: "gray",
     borderWidth: 1,
-    marginLeft: 10
+    paddingHorizontal: 10
+  },
+  logoContainer: {
+    height: 50,
+    width: 100
+  },
+  logoStyles: {
+    height: 50,
+    width: 100
   }
 });
